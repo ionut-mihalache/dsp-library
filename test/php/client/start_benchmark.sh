c0905b8fe5e8e929d513f6d10e3bd559a8e32db9
Ionut Mihalache <ipopescu46@gmail.com>
2026-02-05 15:18:28 +0000

    [update]: Improve script for plot generation
    Signed-off-by: Ionut Mihalache <ipopescu46@gmail.com>

--- test/php/client/start_benchmark.sh
+++ test/php/client/start_benchmark.sh
@@ -14,8 +14,8 @@ if [ ! -d "metrics3" ]; then
 fi
 
 source venv/bin/activate
-python3 create_combined_plots.py ./metrics2 benchmark_results/clients/1770282471/client_benchmark.csv uds/benchmark_results/clients/1770282267/client_benchmark.csv
-python3 create_combined_plots.py ./metrics3 benchmark_results/clients/1770282471/client_benchmark.csv uds/benchmark_results/clients/1770282267/client_benchmark.csv
+python3 create_combined_plots.py ./metrics2 benchmark_results/clients/$lastAQUAbenchmark/client_benchmark.csv uds/benchmark_results/clients/$lastUDSbenchmark/client_benchmark.csv
+python3 create_combined_plots.py ./metrics3 benchmark_results/clients/$lastAQUAbenchmark/client_benchmark.csv uds/benchmark_results/clients/$lastUDSbenchmark/client_benchmark.csv zeromq/benchmark_results/clients/$lastZMQbenchmark/client_benchmark.csv
 deactivate
 
 # ./benchmark.sh /home/$(whoami)/FlameGraph /home/$(whoami)/async-profiler $(pwd) $(date +%s) 128 256 384 512 640 758 886 1024
