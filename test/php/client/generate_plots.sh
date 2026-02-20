abcb3d9a65645a70849112f0f95cdd81b9f21ee0
Ionut Mihalache <ipopescu46@gmail.com>
2026-02-20 12:03:21 +0000

    [new]: Add new plot generation script
    Signed-off-by: Ionut Mihalache <ipopescu46@gmail.com>

---
+++ test/php/client/generate_plots.sh
@@ -0,0 +1,15 @@
+lastAQUAbenchmark=$(ls benchmark_results/clients/ | grep -o "[0-9]\+" | sort -rn | head -n 1)
+lastUDSbenchmark=$(ls uds/benchmark_results/clients/ | grep -o "[0-9]\+" | sort -rn | head -n 1)
+lastZMQbenchmark=$(ls zeromq/benchmark_results/clients/ | grep -o "[0-9]\+" | sort -rn | head -n 1)
+
+if [ ! -d "metrics2" ]; then
+    mkdir -p metrics2
+fi
+if [ ! -d "metrics3" ]; then
+    mkdir -p metrics3
+fi
+
+source venv/bin/activate
+python3 create_combined_plots.py ./metrics2 benchmark_results/clients/$lastAQUAbenchmark/client_benchmark.csv uds/benchmark_results/clients/$lastUDSbenchmark/client_benchmark.csv
+python3 create_combined_plots.py ./metrics3 benchmark_results/clients/$lastAQUAbenchmark/client_benchmark.csv uds/benchmark_results/clients/$lastUDSbenchmark/client_benchmark.csv zeromq/benchmark_results/clients/$lastZMQbenchmark/client_benchmark.csv
+deactivate
